b54fba75bcc85a91279590a60e57a46f95c905d5
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-07-09 16:46:48 -0400

    added divs with js working to modify grid with css

---
+++ etch-a-sketch.js
@@ -0,0 +1,9 @@
+for (let i=0; i<25; i++) {
+
+const container = document.querySelector('#container');
+     const grid = document.createElement('div');
+     grid.classList.add('grid');
+    
+     container.appendChild(grid);
+
+}
